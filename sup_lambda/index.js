819a5d2fe014baae2abbbedd4bfdb971eeee5192
ƘДϑîƘД ✞Дϑυî <kavikat@users.noreply.github.com>
2018-03-17 08:41:41 -0700

    logic
    query added to lambda

--- sup_lambda/index.js
+++ sup_lambda/index.js
@@ -10,21 +10,40 @@ var assert = require('assert'),
 exports.handler = function (event, context, callback) {
 
     var body = event.currentIntent.slots.condition.toLowerCase();
-
     /*
 
     */
     async.waterfall([
-            function (callback) {
+        function (callback) {
 
-                //TODO - logic for sup, api call to foursquare places API
+            //TODO - logic for sup, api call to foursquare places API
+            const request = require('request');
 
-            },
-            function (output, callback) {
-                console.log(output);
-                callback(null, output);
-            },
-        ],
+            request({
+                url: 'https://api.foursquare.com/v2/venues/explore',
+                method: 'GET',
+                qs: {
+                    client_id: process.env.CLIENT_ID,
+                    client_secret: process.env.CLIENT_SECRET,
+                    ll: '40.7243,-74.0018',
+                    query: 'coffee',
+                    v: '20170801',
+                    limit: 1
+                }
+            }, function (err, res, body) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log(body);
+                }
+            });
+
+        },
+        function (output, callback) {
+            console.log(output);
+            callback(null, output);
+        },
+    ],
         function (err, result) {
             console.log(result);
             callback(null, result);
