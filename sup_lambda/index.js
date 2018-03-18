1b9e1967a331580080c1f2fd0a15388d88b31b0a
ƘДϑîƘД ✞Дϑυî <kavikat@users.noreply.github.com>
2018-03-18 08:04:27 -0700

    logic
    variables are not init, need to abstract from promise

--- sup_lambda/index.js
+++ sup_lambda/index.js
@@ -13,37 +13,68 @@ exports.handler = function (event, context, callback) {
 
     var location = event.currentIntent.slots.location;
     /*
-
+        request to foursquare API (trending)
     */
-    request({
-        url: 'https://api.foursquare.com/v2/venues/trending',
-        method: 'GET',
-        qs: {
-            client_id: process.env.CLIENT_ID,
-            client_secret: process.env.CLIENT_SECRET,
-            near: location,
-            radius: 2000,
-            limit: 1,
-            v: '20170801'
-        }//qs
-    }, function (err, res, body) {
-        if (err) {
-            console.error(err);
-        } else {
-            //console.log(body);
-            var heads = body.hereNow,
-                place = body.name;
-            output = {
-                "dialogAction": {
-                    "type": "Close",
-                    "fulfillmentState": "Fulfilled",
-                    "message": {
-                        "contentType": "PlainText",
-                        "content": "There is "+heads+" checked in at "+place+", you might wanna check it out."
-                    }//message
-                }//dialogAction
-            };//output
+    async.waterfall([
+        function (callback) {
+
+            request({
+                url: 'https://api.foursquare.com/v2/venues/trending',
+                method: 'GET',
+                qs: {
+                    client_id: process.env.CLIENT_ID,
+                    client_secret: process.env.CLIENT_SECRET,
+                    near: location,
+                    radius: 2000,
+                    limit: 1,
+                    v: '20170801'
+                }//qs
+            }, function (err, res, body) {
+                if (err) {
+                    console.log("This is an error message: " + err);
+                    output = {
+                        "dialogAction": {
+                            "type": "Close",
+                            "fulfillmentState": "Fulfilled",
+                            "message": {
+                                "contentType": "PlainText",
+                                "content": 'Sorry, I can\'t find anything happening in ' + location
+                            } //msg
+                        } //dA
+                    } //output
+                    console.log(output);
+                    callback(null, output);
+                } else {
+                    console.log(body);
+                    var place = body.response.venues["0"].name,
+                        heads = body.response.venues["0"].hereNow.count;
+
+                    output = {
+                        "dialogAction": {
+                            "type": "Close",
+                            "fulfillmentState": "Fulfilled",
+                            "message": {
+                                "contentType": "PlainText",
+                                "content": "There is " + heads + " checked in at " + place + ", you might wanna check it out."
+                            }//message
+                        }//dialogAction
+                    };//output
+                    console.log("Output: " + output);
+                    callback(null, output);
+                }//else
+            });//request
+
+        },
+        function (output, callback) {
             console.log(output);
-        }//else
-    });//request
+            callback(null, output);
+        },
+    ],
+        function (err, result) {
+            console.log(result);
+            callback(null, result);
+            assert.equal(err, null);
+        }
+    );
+
 };//handler
